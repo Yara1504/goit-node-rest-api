caeb39bb2cbdb538909155809ad985889eef3a7b
Yara1504 <baykenichs@gmail.com>
2024-01-25 11:08:08 +0200

    finally push

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -7,35 +7,34 @@ import HttpError from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res, next) => {
     const allContacts = await contactsService.listContacts();
-    res.status(200).json(allContacts);
+    res.json(allContacts);
 };
 
 export const getOneContact = async (req, res, next) => {
     const { id } = req.params;
     const contact = await contactsService.getContactById(id);
     if (contact) {
-        res.status(200).json(contact);
+        res.json(contact);
     }
-    throw HttpError(404, error.message);
+    throw HttpError(404);
 };
 
 export const deleteContact = async (req, res, next) => {
     const { id } = req.params;
     const deleteContact = await contactsService.removeContact(id);
     if (deleteContact) {
-        res.status(200).json(deleteContact);
+        res.json(deleteContact);
     }
-    throw HttpError(404, error.message); 
+    throw HttpError(404); 
 };
 
 export const createContact = async (req, res, next) => {
-    const { name, email, phone } = req.body;
-    await createContactSchema.validateAsync({ name, email, phone });
 
-    const addContact = await contactsService.addContact(name, email, phone);
-    if (addContact) {
-        res.status(201).json(addContact);
+    const newContact = await contactsService.addContact(req.body);
+    if (newContact) {
+        res.json(newContact);
     }
+    throw HttpError(400);
 };
 
 export const updateContact = async (req, res, next) => {
@@ -43,12 +42,11 @@ export const updateContact = async (req, res, next) => {
     const data = req.body;
     
     if (Object.keys(data).length === 0) {
-        res.status(400).json({ "message": "Body must have at least one field" });
+        res.json({ "message": "Body must have at least one field" });
     }
-    await updateContactSchema.validateAsync(data);
 
-    const updateContact = await contactsService.updateContact(id, data);
+    const changeContact = await contactsService.updateContact(id, data);
     if (updateContact) {
-        res.status(200).json(updateContact);
+        res.json(changeContact);
     }
 };
